080508c24ee021f917af15781439c573657ae8a2
Bezawit <bezawitedilu@gmail.com>
2022-04-08 23:52:27 +0300

    hey

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,12 @@
+57 ; m++)
+                {
+                        putchar(n);
+                        putchar(m);
+
+                        if ((n != 57) || (m != 57))
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+                }
+
