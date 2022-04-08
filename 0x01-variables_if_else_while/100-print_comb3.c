2858b5dda090b66054855c932ddedf5bc83e4166
Bezawit <bezawitedilu@gmail.com>
2022-04-08 23:53:50 +0300

    hey

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,12 +1,33 @@
-57 ; m++)
-                {
-                        putchar(n);
-                        putchar(m);
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 
-                        if ((n != 57) || (m != 57))
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ * No Description:
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
