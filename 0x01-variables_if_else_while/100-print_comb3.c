7edeeeac541ada5c8ef0298b743ac5607d777cb6
Bezawit <bezawitedilu@gmail.com>
2022-04-08 23:58:37 +0300

    hey

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,33 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
+/*
+ * File: 100-print_comb3.c
+ * Auth: Brennan D Baraban
+ */
 
-/* betty style doc for function main goes there */
+#include <stdio.h>
+
 /**
- * main - main function
- * No Description:
- * Return: 0
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int n;
+	int digit1, digit2;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
+
+	putchar('\n');
+
 	return (0);
 }
