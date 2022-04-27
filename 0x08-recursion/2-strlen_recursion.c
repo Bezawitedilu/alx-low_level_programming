8ae752542423d25ec14a2ef0ef39498774f1999d
Bezawit <bezawitedilu@gmail.com>
2022-04-27 20:27:15 +0300

    commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- *  *_strlen_recursion - returns length of a string
- *   *
- *    *@s:string
- *     *
- *      *Return:length
- *       *
- *        */
-int _strlen_recursion(char *s);
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer the string
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
 {
-if (!*s)
+int n = 0;
+
+if (*s > '\0')
 {
-return (0)	
+	n += _strlen_recursion(s + 1) + 1;
 }
-return (1 + _strlen_recursion(++s));
+
+return (n);
 }
