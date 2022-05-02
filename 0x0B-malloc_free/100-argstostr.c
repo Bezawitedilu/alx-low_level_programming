92af5aaff4f2fbf5153cc499d9172a24857d6e7b
Bezawit <bezawitedilu@gmail.com>
2022-05-02 22:42:49 +0300

    commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,97 +1,49 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-/**
- *word_len - finds the length of a word
- *@str:string to test
- *
- *Return:int
- */
-int word_len(char *str)
-{
-	int i = 0, len = 0;
 
-	while (*(str + i) && *(str + i) != ' ')
-	{
-		len++;
-		i++;
-	}
-	return (len);
-}
 /**
- *word_count - counts the number of words
+ * argstostr - a function that concatenates
+ *             all the arguments
  *
- *@str:input
+ * @ac: argument counter
+ * @av: argument holder
  *
- *Return:(no. of words)
- *
- */
-int word_count(char *str)
-{
-	int i = 0, len = 0, count = 0;
+ * Return: a pointer to a new string
+ *         or NULL if it fails
+*/
 
-	for (i = 0; *(str + i); i++)
-	{
-		len++;
-	}
-	for (i = 0; i < len; i++)
-	{
-		if (*(str + i) != ' ')
-		{
-			count++;
-			i += word_len(str + i);
-		}
-	}
-	return (count);
-}
-/**
- *strtow - splits a string into words
- *
- *@str:input
- *
- *Return:0 - success
- *
- */
-char **strtow(char *str)
+char *argstostr(int ac, char **av)
 {
-	int i, words, w, letters, l;
-	char **p;
+	int i, j, k, length;
+	char *str;
 
-	if (str == NULL || str[0] == '\0')
-	{
+	if (ac == 0 || av == NULL)
 		return (NULL);
-	}
-	words = word_count(str);
-	if (words == 0)
+
+	/*find length of vector + '\0' which makes it a 2d array*/
+	length = 0;
+	for (i = 0; i < ac; i++)
 	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			length++;
+		length++;
+	}
+
+	str = malloc((length + 1) * sizeof(char));
+	if (str == NULL)
 		return (NULL);
-	}
-	p = malloc(sizeof(char *) * (words + 1));
-	if (p == NULL)
+
+	k = 0;
+	for (i = 0; i < ac; i++)
 	{
-		return (NULL);
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
 	}
-	for (i = 0; i < words; i++)
-	{
-		while (*(str + w) == ' ')
-		{
-			w++;
-		}
-		letters = word_len(str + w);
-		p[i] = malloc(sizeof(char) * (letters + 1));
-		if (p[i] == NULL)
-		{
-			for (; i >= 0; i--)
-				free(p[i]);
-			free(p);
-			return (NULL);
-		}
-		for (l = 0; l < letters; l++)
-		{
-			p[i][l] = str[w++];
-		}
-		p[i][l] = '\0';
-	}
-	p[i] = NULL;
-	return (p);
+	str[k] = '\0';
+
+	return (str);
 }
